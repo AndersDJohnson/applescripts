d62e7eb640724472a3d585731072036f445f78b8
Anders D. Johnson <anders.d.johnson.developer@gmail.com>
2015-09-13 17:10:12 -0500

    Add build script to make apps from scripts.

---
+++ build.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+FILES=(Scripts/**/*.applescript)
+
+#mkdir -p ./Applications
+
+OIFS="$IFS"
+IFS=$'\n'
+for FILE in `find . -type f -name "*.applescript"`; do
+  echo "FILE: '${FILE}'"
+  OUTFILE=$(echo "$FILE" | sed 's/\.applescript$/.app/')
+  OUTFILE=$(echo "$OUTFILE" | sed 's/^.\/Scripts/~\/Applications/')
+  echo "OUT: '$OUTFILE'"
+  OK="n"
+  if [ -e "$OUTFILE" ]; then
+    read -p "Replace \"$OUTFILE\"? [yn] " -n 1 OK
+    echo
+  else
+    OK="y"
+  fi
+  if [[ $OK = "y" ]]; then
+    osacompile -o "$OUTFILE" "$FILE"
+    echo "Compiled \"$FILE\" to \"$OUTFILE\""
+  else
+    echo "Skipping \"$FILE\""
+  fi
+done
+
+IFS="$OIFS"
+
